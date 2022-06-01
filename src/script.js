810ad949479fb3e3e7a6807456af88770e2c92e5
alex2231 <106402311+alex2231@users.noreply.github.com>
2022-06-01 09:00:12 +0300

    Update time

--- src/script.js
+++ src/script.js
@@ -1,14 +1,39 @@
+function showDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes},`;
+}
+
 function showTemperature(response) {
   let temp = document.querySelector(".number");
   let city = document.querySelector("#city");
   let description = document.querySelector("#description");
   let wind = document.querySelector("#wind-number");
   let humidity = document.querySelector("#humidity-number");
+  let date = document.querySelector("#date");
   temp.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   wind.innerHTML = Math.round(response.data.wind.speed);
   humidity.innerHTML = response.data.main.humidity;
+  date.innerHTML = showDate(response.data.dt * 1000);
 }
 
 let city = "Paris";
